af0a4a1e1b50ee027ca0ef5ce3c44954bbebf01f
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2013-01-16 09:00:17 +0100

    update version kukariri backbonejs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
+  before_filter :set_locale
   protect_from_forgery
+
+
+  def set_locale
+    lang = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    if lang.match /^(en|fr)$/
+      I18n.locale = lang
+    else
+      I18n.locale = 'en'
+    end
+  end
 end
