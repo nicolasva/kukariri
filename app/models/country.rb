0825260fe68c9f53b4428405122b81878a8c0c2f
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2013-01-24 16:34:35 +0100

    update version backbonejs kukariri

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,16 +1,37 @@
 class Country
-  def self.find_country(name)
-    country = Country.named(name)
+  def self.show_country(name)
+    # "Tunisia".scan(/#{val}/)
+    country = Carmen::Country.named(name)
     return country.name
   end
 
-  def self.find_all_country
+  def self.find_country(name)
+    tab_country = Array.new
+    self.all_country.each do |value|
+      value.each do |key, value|
+        unless value.scan(/#{val}/).empty?
+          hash_country = Hash.new
+          hash_country["country"] = value
+          tab_country.push(hash_country)
+        end
+      end
+    end
+    return tab_country
+  end
+
+  def self.all_country
     country = Carmen::Country.all
-    return country
+    return self.hash_all_country(country)
   end
 
   private
-  def hash_all_country(country)
-    
+  def self.hash_all_country(country)
+    tab_country = Array.new
+    country.each do |key,value|
+      hash_country = Hash.new
+      hash_country["country"] = key.name
+      tab_country.push(hash_country)
+    end
+    return tab_country
   end
 end
