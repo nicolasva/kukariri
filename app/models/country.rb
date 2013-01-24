62962f0b246e634ff05ced88d9d49f0aeb500ab8
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2013-01-24 17:27:36 +0100

    update version kukariri backbonejs

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,37 +1,38 @@
 class Country
-  def self.show_country(name)
-    # "Tunisia".scan(/#{val}/)
-    country = Carmen::Country.named(name)
-    return country.name
-  end
+  class << self
+    def show_country(name)
+      # "Tunisia".scan(/#{val}/)
+      country = Carmen::Country.named(name)
+      return country.name
+    end
 
-  def self.find_country(name)
-    tab_country = Array.new
-    self.all_country.each do |value|
-      value.each do |key, value|
-        unless value.capitalize.scan(/#{name.capitalize}/).empty?
-          hash_country = Hash.new
-          hash_country["country"] = value
-          tab_country.push(hash_country)
+    def find_country(name)
+      tab_country = Array.new
+      all_country.each do |value|
+        value.each do |key, value|
+          unless value.capitalize.scan(/#{name.capitalize}/).empty?
+            hash_country = Hash.new
+            hash_country["country"] = value
+            tab_country.push(hash_country)
+          end
         end
       end
+      return tab_country
     end
-    return tab_country
-  end
 
-  def self.all_country
-    country = Carmen::Country.all
-    return self.hash_all_country(country)
-  end
-
-  private
-  def self.hash_all_country(country)
-    tab_country = Array.new
-    country.each do |key,value|
-      hash_country = Hash.new
-      hash_country["country"] = key.name
-      tab_country.push(hash_country)
+    def all_country
+      country = Carmen::Country.all
+      return hash_all_country(country)
     end
-    return tab_country
+
+    def hash_all_country(country)
+      tab_country = Array.new
+      country.each do |key,value|
+        hash_country = Hash.new
+        hash_country["country"] = key.name
+        tab_country.push(hash_country)
+      end
+      return tab_country
+    end
   end
 end
