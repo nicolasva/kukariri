63871d483621b04aef34d3f4cee991818c70d643
nicolas.vandenbogaerde@gmail.com <nicolas.vandenbogaerde@gmail.com>
2013-03-20 09:37:22 +0100

    update version .rvmrc to .ruby-version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def be_backbone?
-    Rails.logger.info request.path
     request.path.scan(/^.{1,}(#).{1,}$/) == Array.new && request.path != "/" ? true : false
   end
 end
